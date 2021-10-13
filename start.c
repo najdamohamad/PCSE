cf100af4b6d5f86afb9b2fcb873d8640a0bce988
Mohamad Najda <mohamad.najda@grenoble-inp.org>
2021-10-13 10:20:10 +0200

    final gestion decran with trait car et get cursor from vin

--- start.c
+++ start.c
@@ -55,53 +55,130 @@ void place_curseur(uint32_t lig, uint32_t col)
     outb( (uint8_t)(p >> 8) , 0x3D5);
 }
 
-void traite_car(char c,uint32_t lig, uint32_t col)
+
+
+// void traite_car(char c,uint32_t lig, uint32_t col)
+// {
+//   uint8_t acii = (uint8_t)c ;
+//   if(acii > 31 && acii < 127)
+//   {
+//     ecrit_car(lig,col,c,15,0);
+//   }
+//   else
+//   {
+//     switch(acii)
+//     {
+//       case 8:
+//         if(col != 0)
+//         {
+//           place_curseur(lig , col -1);
+//         }
+//         break;
+//
+//       case 9:
+//           if(col < 72)
+//           {
+//             place_curseur(lig , col+8);
+//           }
+//
+//         break;
+//
+//
+//     case 10:
+//         if(lig != 24)
+//         {
+//           place_curseur(lig +1 , 0);
+//         }
+//         break;
+//
+//     case 12:
+//           efface_ecran();
+//           place_curseur(0,0);
+//
+//         break;
+//     case 13:
+//
+//         place_curseur(lig,0);
+//         break;
+//     }
+//   }
+// }
+//
+
+//vini part
+
+#define NLIG 25
+#define NCOL 80
+
+
+uint16_t trouve_curseur(void)
 {
-  uint8_t acii = (uint8_t)c ;
-  if(acii > 31 && acii < 127)
-  {
-    ecrit_car(lig,col,c,15,0);
-  }
-  else
-  {
-    switch(acii)
-    {
-      case 8:
-        if(col != 0)
-        {
-          place_curseur(lig , col -1);
-        }
-        break;
+    uint16_t pos;
 
-      case 9:
-          if(col < 72)
-          {
-            place_curseur(lig , col+8);
-          }
+    outb(0x0E, 0x3D4);
+    pos = inb(0X3D5) << 8;
+    outb(0x0F, 0x3D4);
+    pos += inb(0X3D5);
 
-        break;
-
-
-    case 10:
-        if(lig != 24)
-        {
-          place_curseur(lig +1 , 0);
-        }
-        break;
-
-    case 12:
-          efface_ecran();
-          place_curseur(0,0);
-
-        break;
-    case 13:
-
-        place_curseur(lig,0);
-        break;
-    }
-  }
+    return pos;
 }
 
+void traite_car(char c)
+{
+    uint8_t ascii = (uint8_t)c;
+    uint16_t lig, col, pos;
+
+    pos = trouve_curseur();
+    lig = pos / NCOL;
+    col = pos % NCOL;
+
+    if( 31 < ascii && ascii < 127 ) {
+        ecrit_car(lig, col, c ,7 , 0);
+        if (col < NCOL) {
+            place_curseur(lig, col+1);
+        } else {
+            place_curseur(lig+1, 0);
+        }
+    } else {
+        switch(ascii) {
+            case 8:
+                if (col != 0) {
+                    place_curseur(lig, col-1);
+                }
+                break;
+
+            case 9:
+                if ((col % 8) != 0 && col != (NCOL - 1)) {
+                    col = col - (col%8) + 8;
+                    if(col < NCOL)
+                        place_curseur(lig, col);
+                    else
+                        place_curseur(lig, NCOL-1);
+                }
+                break;
+
+            case 10:
+                if (lig != NLIG - 1)
+                    place_curseur(lig + 1, 0);
+                break;
+
+            case 12:
+                efface_ecran();
+                place_curseur(0,0);
+                break;
+
+            case 13:
+                place_curseur(lig, 0);
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+
+//mydefi
 void defilement(void)
 {
   void* Destination ;
@@ -121,13 +198,22 @@ void defilement(void)
   }
 }
 
- void console_putbytes(const char *s, int len)
+void console_putbytes(char *chaine, int32_t taille)
 {
-
-
+    traite_car('\f'); //effacer ecran
+    for (int32_t i = 0; i < taille; i++){
+        traite_car(chaine[i]);
+    }
 }
 
-
+//  void console_putbytes(const char *s, int len)
+// {
+//     // On cherche la position du place_curseur
+//
+//
+// }
+//
+//
 
 
 
@@ -143,16 +229,18 @@ void kernel_start(void)
   //traite_car('\n',0,15);
     // quand on saura gerer l'ecran, on pourra afficher x
    //efface_ecran () ;
-   for (int i = 0 ; i< 25 ; i++)
-   {
-     for ( int j = 0; j<80 ; j++)
-     {
-       ecrit_car(i,j,'m',7,0);
-     }
-   }
-
-
-   defilement();
+   // for (int i = 0 ; i< 25 ; i++)
+   // {
+   //   for ( int j = 0; j<80 ; j++)
+   //   {
+   //     ecrit_car(i,j,'m',7,0);
+   //   }
+   // }
+   char* name ;
+   name = "mohamad" ;
+   console_putbytes(name,7/*strlen(name)*/);
+   
+   //defilement();
     //ecrit_car(5,10,'K',15,0);
     //ecrit_car(15,40,'K',15,0);
 
